cb6c82356ea85b614b54ce1552f7a61380158b70
marceloquevedo <marceloquevedo@mbp-de-marcelo>
2024-11-24 20:47:29 -0300

    Primeiros ajustes

---
+++ src/main/java/com/br/fiap/postech/soat7grupo5_pagamento/Soat7grupo5PagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soat7grupo5PagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat7grupo5PagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/postech/soat7grupo5_pagamento/Soat7grupo5PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.postech.soat7grupo5_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Soat7grupo5PagamentoApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
